dfa0d5db12154b68d92f9f770e6b632a4eaf922f
theblackblazer <singhaniapiyush294@gmail.com>
2017-08-16 03:13:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req,res){
-    res.send('Article Two Requested And Will Be Served Here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function (req,res){
-    res.send('Article Three Requested And Will Be Served Here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
