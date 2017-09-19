956c5792922f694cebf8675c2d1d2b2a09d61b81
theblackblazer <singhaniapiyush294@gmail.com>
2017-09-19 16:53:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,31 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit=document.getElementById('Submit_btn');
 submit.onclick=function(){
-    //Make A request to the server and send the name
+     //Create A Request
+    var request = new XMLHttpRequest();
+    //Capture the variable and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take Some Action
+            if(request.status===200){
+               //Make A request to the server and send the name
     
     //Capture a list of names and render it as a list
-    var names[]=['name1','name2','name3','name4'];
+    var names=request.responseText;
+    names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++){
         list+= '<li>' +names[i]+ '</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+            }
+            
+        }
+    };
+    //Make A Request
+    request.open('GET','http://singhaniapiyush294.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+   
+    
 };
