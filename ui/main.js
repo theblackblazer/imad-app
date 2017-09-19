af3c2fba4ea7dc2cbd750b7c8825db1c01072530
theblackblazer <singhaniapiyush294@gmail.com>
2017-09-19 07:00:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //move the image
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick = function(){
-    img.style.marginLeft='100px';
+   var Interval=setInterval(moveRight,50);
 };
