0d5269e85d76daa2a98dc148ca1e95f4a610b475
theblackblazer <singhaniapiyush294@gmail.com>
2017-09-19 12:42:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,13 @@
-console.log('Loaded!');
-//move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function(){
-   var Interval=setInterval(moveRight,50);
+//Counter code
+var button = document.getElementbyId("counter");
+button.onclick=function(){
+    
+    //Create A Request
+    
+    //Capture the variable and store it in a variable
+    
+    //Render the variable to the correct span
+    counter = counter+1;
+    var span = document.getElementById("count");
+    span.innerHTML=counter.toString();
 };
