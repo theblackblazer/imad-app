09dbe170c9f55e17461fe4582c626b251d2d0789
theblackblazer <singhaniapiyush294@gmail.com>
2017-09-19 13:06:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,22 @@ var counter=0;
 button.onclick= function() {
     
     //Create A Request
-    
+    var request = new XMLHttpRequest();
     //Capture the variable and store it in a variable
-    
-    //Render the variable to the correct span
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take Some Action
+            if(request.status===200){
+                var counter=request.responseText;
+                 
+                var span = document.getElementById('count');
+                 span.innerHTML=counter.toString();
+            }
+            
+        }
+    };
+    //Make A Request
+    request.open('GET','http://singhaniapiyush294.imad.hasura-app.io/counter',true);
+    request.send(null);
+   
 };
