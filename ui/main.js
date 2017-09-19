24a39d0c339a1c34e8ba5a50e2285add3a10e39f
theblackblazer <singhaniapiyush294@gmail.com>
2017-09-19 16:36:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,21 @@ button.onclick= function() {
     request.send(null);
    
 };
+
+//Submit Name
+
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('Submit_btn');
+submit.onclick=function(){
+    //Make A request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names[]=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li>' +names[i]+ '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
